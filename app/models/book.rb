89798519446adea7b4a99358cdc5423e48e6b4f4
gregspagnolo <dwdrummer@gmail.com>
2016-07-11 18:39:28 -0500

    Add book genre relationships

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ActiveRecord::Base
 
+	has_many :book_genres
+	has_many :genres, through: :book_genres
+
 	scope :finished, ->{ where.not(finished_on: nil)}
 	scope :recent, ->{ where('finished_on > ?', 7.days.ago)}
 	scope :search, ->(keyword) { where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present? }
